c8c70e5a0a4ede587dd19d1a47a9889a9658dcad
Roman Inflianskas <infroma@gmail.com>
2020-05-10 15:13:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ packages = \
 package_data = \
 {'': ['*']}
 
-install_requires = \
-['pynndb>=1.1.5,<2.0.0']
-
 setup_kwargs = {
     'name': 'legdb',
     'version': '0.1.0',
@@ -22,7 +19,6 @@ setup_kwargs = {
     'url': None,
     'packages': packages,
     'package_data': package_data,
-    'install_requires': install_requires,
     'python_requires': '>=3.6,<4.0',
 }
 
